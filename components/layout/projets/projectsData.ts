1dc16f60ee43c4690c10c94e8a9a244696a776ca
TxMy <to.doguet@gmail.com>
2025-04-24 15:28:12 +0200

    add: link

--- components/layout/projets/projectsData.ts
+++ components/layout/projets/projectsData.ts
@@ -15,7 +15,7 @@ export const personalProjects = [
   {
     title: "camping_API",
     description: "Camping API est une application backend développée avec Spring Boot, proposant une API RESTful sécurisée pour gérer les données d'un site de camping : comptes utilisateurs, réservations et équipements, le tout connecté à une base MySQL via JPA.",
-    link: "https://github.com",
+    link: "https://github.com/TxMy-Elu/camping_api",
     technologies: [
       { name: "Spring Boot", icon: "/icons/tech/lg/spring-3.svg" },
       { name: "Java", icon: "/icons/tech/lg/java-14.svg" }
@@ -24,7 +24,7 @@ export const personalProjects = [
   {
     title: "camping_WEB",
     description: "Propulsée par React, notre application web vous offre performance, simplicité et personnalisation, pour une gestion optimale de votre présence professionnelle.",
-    link: "https://github.com",
+    link: "github.com/TxMy-Elu/camping_web",
     technologies: [
       { name: "React", icon: "/icons/tech/lg/react-2.svg" },
       { name: "Tailwind CSS", icon: "/icons/tech/lg/tailwindcss.svg" }
@@ -33,7 +33,7 @@ export const personalProjects = [
   {
     title: "camping_APP",
     description: "L’Application de Gestion de Camping est une solution desktop Java dotée d’une interface moderne avec JavaFX et MaterialFX, permettant de gérer efficacement les activités, les animateurs et l’authentification des utilisateurs d’un site de camping.",
-    link: "https://github.com",
+    link: "https://github.com/TxMy-Elu/camping_app",
     technologies: [
       { name: "Java", icon: "/icons/tech/lg/java-14.svg" }
     ]
@@ -41,7 +41,7 @@ export const personalProjects = [
   {
     title: "API_Cafe",
     description: "apiCafe est une application Laravel RESTful dédiée à la gestion des dosettes de café. Elle permet de référencer les cafés par marque, pays d’origine et prix.",
-    link: "https://github.com",
+    link: "https://github.com/TxMy-Elu/cafe_API",
     technologies: [
       { name: "Laravel", icon: "/icons/tech/lg/laravel-2.svg" },
     ]
@@ -49,7 +49,7 @@ export const personalProjects = [
   {
     title: "Web_Cafe",
     description: "WebCafe est une application frontend développée avec Next.js, permettant aux utilisateurs de consulter et filtrer les dosettes de café disponibles par marque, pays d’origine et prix. L’interface moderne et réactive offre une expérience fluide pour explorer les références proposées.",
-    link: "https://github.com",
+    link: "https://github.com/TxMy-Elu/cafe_web",
     technologies: [
         { name: "Next Js", icon: "/icons/tech/lg/next-js.svg" },
         { name: "Tailwind CSS", icon: "/icons/tech/lg/tailwindcss.svg" }
