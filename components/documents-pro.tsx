1056f6268df7ec77f0a721e6bc018a3e089fb96a
TxMy <to.doguet@gmail.com>
2025-11-20 10:56:42 +0100

    Update documents-pro.tsx

--- components/documents-pro.tsx
+++ components/documents-pro.tsx
@@ -2,13 +2,13 @@ import { FaFileAlt, FaFileContract } from "react-icons/fa";
 
 export default function DocumentsPro() {
   return (
-    <div className="backdrop-blur-sm bg-white/30 border border-gray-200 rounded-2xl p-8 mb-6">
+    <div className="backdrop-blur-sm bg-white/30 border border-gray-200 rounded-2xl p-8 mb-6 max-w-2xl mx-auto">
       <h2 className="text-3xl font-bold text-gray-800 mb-8 relative">
         Documents Professionnels
         <span className="absolute bottom-0 left-0 w-20 h-1 bg-blue-500 rounded-full"></span>
       </h2>
 
-      <div className="grid gap-6">
+      <div className="grid gap-6 place-items-center">
         <a
           href="/documents/CV_Tom_Doguet.pdf"
           target="_blank"
